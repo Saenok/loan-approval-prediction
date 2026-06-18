42bf34be29a46dc8af5e07079c27535124bfba5b
Saenok <108672471+Saenok@users.noreply.github.com>
2026-06-18 12:33:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,18 @@ st.title("Loan Approval Prediction")
 
 loan_id = st.number_input("Loan ID", min_value=1)
 no_of_dependents = st.number_input("Number of Dependents", min_value=0)
-education = st.selectbox("Education", [0, 1])
-self_employed = st.selectbox("Self Employed", [0, 1])
+education_text = st.selectbox(
+    "Education",
+    ["Graduate", "Not Graduate"]
+)
 
+self_employed_text = st.selectbox(
+    "Self Employed",
+    ["Yes", "No"]
+)
+education = 0 if education_text == "Graduate" else 1
+
+self_employed = 1 if self_employed_text == "Yes" else 0
 income_annum = st.number_input("Annual Income")
 loan_amount = st.number_input("Loan Amount")
 loan_term = st.number_input("Loan Term")
